98b0506666a93ba5cc04ce997cd318148ae1fffe
falcon-hayaki <allrightprincekin@outlook.com>
2022-07-24 12:37:46 +0800

    fix bug

--- bot.py
+++ bot.py
@@ -51,8 +51,6 @@ handler = RotatingFileHandler('logs/schedulerLog.txt', 'a', 1024*10, 1, 'utf-8')
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    filename='logs/schedulerLog.txt',
-                    filemode='a',
                     handlers=[handler])
 
 if __name__ == "__main__":
