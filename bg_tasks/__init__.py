ff52ad17871a50c6f35ea6a1501ae56c906b6b74
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-23 18:32:41 +0800

    debug

--- bg_tasks/__init__.py
+++ bg_tasks/__init__.py
@@ -4,11 +4,3 @@ from utils.tz import SHA_TZ
 scheduler = AsyncIOScheduler(timezone=SHA_TZ)
 
 from ._checkin import checkin
-
-async def __start_s():
-    scheduler.start()
-def start_scheduler():
-    asyncio.run(__start_s)
-
-def get_scheduler_jobs() -> list:
-    return scheduler.get_jobs()
--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 from botoy import bot
-from bg_tasks import start_scheduler, get_scheduler_jobs
+from bg_tasks import scheduler
+import asyncio
 
 bot.set_url('127.0.0.1:8086')
 
 bot.load_plugins()
 bot.print_receivers()
 
-print(get_scheduler_jobs())
-start_scheduler()
-
-bot.run()
+async def main():
+    scheduler.start()
+    bot.run()
+    
+asyncio.run(main)
