53a86d3a3b3aaca67a728c87a4d520d6a6e129be
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-23 18:50:26 +0800

    debug

--- bg_tasks/__init__.py
+++ bg_tasks/__init__.py
@@ -1,21 +1,5 @@
-import asyncio
-import threading
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from utils.tz import SHA_TZ
-scheduler = AsyncIOScheduler(timezone=SHA_TZ)
+scheduler = BackgroundScheduler(timezone=SHA_TZ)
 
 from ._checkin import checkin
-
-async def scheduler_start():
-    scheduler.start()
-    while True:
-        await asyncio.sleep(0.1)
-
-def __run_event_loop():
-    new_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(new_loop)
-    new_loop.run_until_complete(scheduler_start())
-    return
-
-def start_bg():
-    threading.Thread(target=__run_event_loop).start()
--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot.set_url('127.0.0.1:8086')
 bot.load_plugins()
 bot.print_receivers()
 
-from bg_tasks import start_bg
-start_bg()
+from bg_tasks import scheduler
+scheduler.start()
 
 bot.run()
