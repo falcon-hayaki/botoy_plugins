b3752ddea0e14568d29ab613eb172df7968c363b
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-23 18:33:28 +0800

    debug

--- bg_tasks/_checkin.py
+++ bg_tasks/_checkin.py
@@ -3,6 +3,6 @@ from botoy import action
 from . import scheduler
 
 @scheduler.scheduled_job('cron', hour='17', minute='*', second='*')
-async def checkin():
+def checkin():
     print('test')
-    await action.sendGroupText(1014696092, '签到')
+    action.sendGroupText(1014696092, '签到')
