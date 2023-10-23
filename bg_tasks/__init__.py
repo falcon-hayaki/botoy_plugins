ef0b42a240a4b431d90c4897bc415e69b11545fc
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-23 18:14:41 +0800

    debug

--- bg_tasks/__init__.py
+++ bg_tasks/__init__.py
@@ -1,6 +1,6 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from utils.tz import SHA_TZ
-scheduler = BackgroundScheduler(timezone=SHA_TZ)
+scheduler = AsyncIOScheduler(timezone=SHA_TZ)
 
 from ._checkin import checkin
 
@@ -9,21 +9,3 @@ def start_scheduler():
 
 def get_scheduler_jobs() -> list:
     return scheduler.get_jobs()
-
-
-
-
-if __name__ == '__main__':
-    scheduler = BackgroundScheduler(timezone=SHA_TZ)
-    
-    @scheduler.scheduled_job('cron', hour='17', minute='*', second='*')
-    def test():
-        print('test')
-        
-    print(scheduler.get_jobs())
-    scheduler.start()
-    
-    import time
-    while True:
-        print(scheduler.get_jobs())
-        time.sleep(1)
