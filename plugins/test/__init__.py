e8706ded67e3eab0a02c5dacac1ef6819846a8e3
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-24 10:23:06 +0800

    debug

--- plugins/test/__init__.py
+++ plugins/test/__init__.py
@@ -1,10 +1,9 @@
-from botoy import ctx, S, mark_recv, Action
+from botoy import ctx, S, mark_recv, action
 
 async def hello():
     if msg := (ctx.g or ctx.f):
         if msg.text == '一花':
             await S.text("ここっすよ～")
-            with Action as action:
-                await action.sendGroupText(1014696092, '哈哈')
+            await action.sendGroupText(1014696092, '哈哈')
 
 mark_recv(hello)
