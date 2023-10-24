8a0bef771ee21b029c420b1ba1e042797aa0617b
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-24 10:12:48 +0800

    debug

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot.set_url('127.0.0.1:8086')
 bot.load_plugins()
 bot.print_receivers()
 
-from bg_tasks import scheduler
-scheduler.start()
+# from bg_tasks import scheduler
+# scheduler.start()
 
 bot.run()
--- plugins/test/__init__.py
+++ plugins/test/__init__.py
@@ -1,8 +1,10 @@
-from botoy import ctx, S, mark_recv
+from botoy import ctx, S, mark_recv, action
 
 async def hello():
     if msg := (ctx.g or ctx.f):
         if msg.text == '一花':
             await S.text("ここっすよ～")
+            with action:
+                await action.sendGroupText(1014696092, '哈哈')
 
 mark_recv(hello)
