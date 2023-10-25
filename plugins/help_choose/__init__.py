9856a3371dab6ec8df9733dd5a691a4ea63a2408
falcon-hayaki <allrightprincekin@outlook.com>
2023-10-25 17:04:31 +0800

    debug

--- plugins/help_choose/__init__.py
+++ plugins/help_choose/__init__.py
@@ -5,7 +5,7 @@ from botoy import ctx, S, mark_recv
 async def help_choose():
     if msg := (ctx.g or ctx.f):
         msg_split = msg.text.strip().split()
-        if msg_split[0] in ['帮我选', '!c', '！c']:
+        if msg_split and msg_split[0] in ['帮我选', '!c', '！c']:
             await S.text(random.choice(msg_split[1:]) or '')
 
 mark_recv(help_choose)
