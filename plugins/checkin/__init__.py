2a56e5f6d8289b0765e53872c197db298da46e3a
falcon-hayaki <allrightprincekin@outlook.com>
2023-11-06 11:09:53 +0800

    debug

--- plugins/checkin/__init__.py
+++ plugins/checkin/__init__.py
@@ -11,6 +11,7 @@ next_check_time = crontab.get_next(datetime)
 print(next_check_time)
 
 async def checkin():
+    global next_check_time
     if msg := ctx.g and not lock.locked():
         async with lock:
             if beijingnow() >= next_check_time:
