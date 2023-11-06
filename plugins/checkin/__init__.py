52731d1a1401f7046209d2f070747f896584fd55
falcon-hayaki <allrightprincekin@outlook.com>
2023-11-06 11:25:06 +0800

    debug

--- plugins/checkin/__init__.py
+++ plugins/checkin/__init__.py
@@ -8,22 +8,19 @@ from utils.tz import beijingnow
 
 lock = asyncio.Lock()
 crontab = croniter('* * * * *', beijingnow())
-crontab_next = copy.deepcopy(crontab.get_next(datetime))
-print(type(crontab_next), crontab_next)
+crontab_next = None
 test1 = datetime.utcnow()
 test2 = '123'
 test3 = beijingnow()
-test4 = copy.deepcopy(crontab.get_next(datetime))
 
 async def checkin():
     print(test1)
     print(test2)
     print(test3)
-    print(test4)
     print(crontab_next)
     if msg := ctx.g and not lock.locked():
         async with lock:
-            if beijingnow() >= crontab_next:
+            if crontab_next is None or beijingnow() >= crontab_next:
                 await action.sendGroupText(1014696092, '签到')
                 crontab_next = crontab.get_next(datetime)
 
