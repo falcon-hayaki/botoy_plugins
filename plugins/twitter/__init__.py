17789ff151714743a0b69b6ba2ef0faeacccfabc
falcon-hayaki <allrightprincekin@outlook.com>
2023-11-07 11:58:16 +0800

    debug

--- plugins/twitter/__init__.py
+++ plugins/twitter/__init__.py
@@ -1,9 +1,9 @@
 from botoy import mark_recv, jconfig
 
-from .get_tweet import get_tweet
-
 if jconfig.get('twitter'):
     from utils.twitter_manager import TwitterManager
     tm = TwitterManager()
+
+    from .get_tweet import get_tweet
     
     mark_recv(get_tweet)
