9eb18d284235a5d4ac6f11c4e1e17f2798664476
falcon-hayaki <allrightprincekin@outlook.com>
2024-07-19 11:52:41 +0800

    debug

--- bot.py
+++ bot.py
@@ -9,21 +9,26 @@ bot.print_receivers()
 
 bot.run()
 
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# async def run_with_reconnect(bot):
-#     while True:
-#         try:
-#             await bot.run()
-#         except Exception as e:
-#             logger.error(f"bot.run() 失败: {e}")
-#             await asyncio.sleep(5)
+async def run_with_reconnect(bot):
+    while True:
+        try:
+            await bot.run()
+        except Exception as e:
+            logger.error(f"bot.run() 失败: {e}")
+            await asyncio.sleep(5)
 
-# def main():
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(run_with_reconnect(bot))
-#     loop.close()
+def main():
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(run_with_reconnect(bot))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
