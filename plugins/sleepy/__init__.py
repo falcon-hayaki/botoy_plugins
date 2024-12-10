808bc40dd6fb01b48ad0014591c6b87e0d0408f3
falcon-hayaki <allrightprincekin@outlook.com>
2024-12-10 17:45:07 +0800

    debug end

--- plugins/sleepy/__init__.py
+++ plugins/sleepy/__init__.py
@@ -9,7 +9,6 @@ async def sleepy():
         if msg.text == '#hayaki似了没' and msg.from_user != jconfig.qq:
             url = 'http://127.0.0.1:9010/query'
             resp = requests.get(url)
-            print(resp.text)
             if resp.status_code == 200 and resp.json().get('success', False):
                 info = resp.json()['info']
                 await S.text("hayaki状态: {}\n{}".format(info['name'], info['desc']))
