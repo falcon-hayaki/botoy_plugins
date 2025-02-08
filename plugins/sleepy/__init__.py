d895ed35cf04c3f378228ef4e4033817cb1ee63a
falcon-hayaki <allrightprincekin@outlook.com>
2025-02-08 11:33:34 +0800

    update: sleepy url

--- plugins/sleepy/__init__.py
+++ plugins/sleepy/__init__.py
@@ -7,7 +7,7 @@ from utils.media_processing import download_from_url_and_convert_to_base64
 async def sleepy():
     if msg := (ctx.g or ctx.f):
         if msg.text.lower() in ['#hayaki', 'hayaki状态', 'hayaki似了没'] and msg.from_user != jconfig.qq:
-            url = 'http://127.0.0.1:9010/query'
+            url = 'http://sleepy.hayaki.icu/query'
             resp = requests.get(url)
             if resp.status_code == 200 and resp.json().get('success', False):
                 info = resp.json()['info']
