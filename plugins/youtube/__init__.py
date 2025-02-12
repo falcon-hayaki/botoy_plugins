711a236cf2de57a4ca85e6d8f9cf486aeedb4b9f
falcon-hayaki <allrightprincekin@outlook.com>
2025-02-12 18:38:52 +0800

    update ytbtimeline_stats

--- plugins/youtube/__init__.py
+++ plugins/youtube/__init__.py
@@ -4,9 +4,10 @@ if jconfig.get('youtube.api_key'):
     from utils.youtube_manager import YoutubeManager
     ym = YoutubeManager()
 
-    from .ytbtimeline import ytbtimeline, ytbtimeline_subs
+    from .ytbtimeline import ytbtimeline, ytbtimeline_subs, ytbtimeline_stats
     from .get_ytb_video import get_ytb_video
     
     mark_recv(ytbtimeline)
     mark_recv(ytbtimeline_subs)
+    mark_recv(ytbtimeline_stats)
     mark_recv(get_ytb_video)
