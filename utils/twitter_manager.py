0e962b5f090420968dd315922ff9d428abf5f2e0
falcon-hayaki <allrightprincekin@outlook.com>
2025-11-24 19:07:45 +0800

    debug

--- utils/twitter_manager.py
+++ utils/twitter_manager.py
@@ -41,13 +41,19 @@ class TwitterManager():
                 "withGrokTranslatedBio": False
             }),
             'features': json.dumps({
-                "hidden_profile_subscriptions_enabled": True,
-                "responsive_web_graphql_exclude_directive_enabled": True,
-                "verified_phone_label_enabled": False,
-                "highlights_tweets_tab_ui_enabled": True,
-                "creator_subscriptions_tweet_preview_api_enabled": True,
-                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
-                "responsive_web_graphql_timeline_navigation_enabled": True
+                "hidden_profile_subscriptions_enabled":True,
+                "profile_label_improvements_pcf_label_in_post_enabled":True,
+                "responsive_web_profile_redirect_enabled":False,
+                "rweb_tipjar_consumption_enabled":True,
+                "verified_phone_label_enabled":False,
+                "subscriptions_verification_info_is_identity_verified_enabled":True,
+                "subscriptions_verification_info_verified_since_enabled":True,
+                "highlights_tweets_tab_ui_enabled":True,
+                "responsive_web_twitter_article_notes_tab_enabled":True,
+                "subscriptions_feature_can_gift_premium":True,
+                "creator_subscriptions_tweet_preview_api_enabled":True,
+                "responsive_web_graphql_skip_user_profile_image_extensions_enabled":False,
+                "responsive_web_graphql_timeline_navigation_enabled":True
             }),
             'fieldToggles': json.dumps({
                 "withAuxiliaryUserLabels": False
